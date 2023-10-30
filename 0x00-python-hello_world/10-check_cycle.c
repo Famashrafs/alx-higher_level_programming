427979d7faa932fd396c236d9bf60f4b10d40c2f
famashrafs <famashraf9@gmail.com>
2023-10-30 15:32:32 -0700

    11

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,14 +9,14 @@
  * */
 int check_cycle(listint_t *list)
 (
- 	listint_t *slow = list ,*fast = list;
+ 	listint_t *slow = list, *fast = list;
 
 	while (fast && fast->next)
 	{
 		slow = slow->next;
-		fast = fast->next;
+		fast = fast->next->next;
 		if(slow == fast)
-		return (1);
+			return (1);
 	}
 	return (0);
 )
